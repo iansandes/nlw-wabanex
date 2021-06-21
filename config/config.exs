f99ec85a20bfb3e031317f3175f6034b15663394
Author Ian Sandes <iansandes15@gmail.com>
2021-06-20 21:34:15 -0300

    feat(wabanex): create project and insert route to calculate imc

---
+++ config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :wabanex,
+  ecto_repos: [Wabanex.Repo]
+
+# Configures the endpoint
+config :wabanex, WabanexWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "YK5VXmwVQ+oJa6Fr7/hW72eF+rpnK+tyzWqnml894C155edrsjUfgHNLSxSUM6Bk",
+  render_errors: [view: WabanexWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: Wabanex.PubSub,
+  live_view: [signing_salt: "nXMl68m2"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
